7aa78940b15ab0401471763c1238b839ef96b1fb
Damien Crier <damien.crier@camptocamp.com>
2016-10-12 14:48:22 +0200

    [ADD] install 'es_ES' language

--- odoo/songs/install/pre.py
+++ odoo/songs/install/pre.py
@@ -48,7 +48,7 @@ def setup_company(ctx):
 @anthem.log
 def setup_language(ctx):
     """ Installing language and configuring locale formatting """
-    for code in ('fr_FR',):
+    for code in ('fr_FR', 'es_ES'):
         ctx.env['base.language.install'].create({'lang': code}).lang_install()
     ctx.env['res.lang'].search([]).write({
         'grouping': [3, 0],
