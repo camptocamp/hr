aed7010ab40ebf58ea61fab1bced8f643467aba7
Damien Crier <damien.crier@camptocamp.com>
2016-10-13 12:00:11 +0200

    [FIX] activate currencies before importing companies

--- odoo/songs/install/csv_import.py
+++ odoo/songs/install/csv_import.py
@@ -9,8 +9,18 @@ from ..common import req
 
 
 @anthem.log
+def activate_currencies(ctx, curr_list=['EUR']):
+    curr_obj = ctx.env['res.currency']
+    for curr in curr_list:
+        curr_obj.with_context(
+            active_test=False).search(
+                [('name', '=', curr)], limit=1).active = True
+
+
+@anthem.log
 def import_companies(ctx):
     """ import company """
+
     content = resource_stream(req, 'data/install/res.company.csv')
     load_csv_stream(ctx, 'res.company', content, delimiter=',')
 
@@ -32,6 +42,7 @@ def import_expense_products(ctx):
 @anthem.log
 def main(ctx):
     """ Main: creating demo data """
+    activate_currencies(ctx, ['GBP', 'EUR', 'USD'])
     import_companies(ctx)
     import_expense_categ(ctx)
     import_expense_products(ctx)
