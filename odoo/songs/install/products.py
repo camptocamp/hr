141cb37374abc39e14d1ea42b2c6442a0b6f3a28
Damien Crier <damien.crier@camptocamp.com>
2017-02-08 11:44:34 +0100

    configure expensable products

---
+++ odoo/songs/install/products.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import anthem
+
+
+@anthem.log
+def configure_expense_products(ctx):
+    """ set sale_ok and purchase_ok to False for expense products"""
+    ctx.env['product.template'].search(
+        [('can_be_expensed', '=', 'True')]).write(
+            {'sale_ok': False, 'purchase_ok': False}
+        )
+
+
+@anthem.log
+def main(ctx):
+    """ configure products """
+    configure_expense_products(ctx)
