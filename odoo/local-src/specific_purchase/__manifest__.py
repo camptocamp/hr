3bb003732fa9a5d8f7ad11f5c34279c90f6b65e8
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-20 17:59:58 +0100

    add base security group

--- odoo/local-src/specific_purchase/__manifest__.py
+++ odoo/local-src/specific_purchase/__manifest__.py
@@ -5,12 +5,19 @@
 {
     "name": "RocTool specific Purchase Module",
     "version": "10.0.1.0.0",
-    "depends": ['purchase'],
+    "depends": [
+        'purchase',
+        'hr',
+    ],
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "http://www.camptocamp.com",
     "license": "GPL-3 or any later version",
     "category": "Sale",
     "data": [
+        # security
+        'security/purchase_security.xml',
+        'security/ir.model.access.csv',
+        # views
         'views/purchase_views.xml',
     ],
     'installable': True,
