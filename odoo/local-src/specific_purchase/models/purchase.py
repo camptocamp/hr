a34ae7e438bf429d22f0afd10fead83627cfa7c6
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-20 13:54:31 +0100

    Activate PO double validation

---
+++ odoo/local-src/specific_purchase/models/purchase.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Author: Denis Leemann
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api, exceptions, _
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def button_approve(self, force=False):
+        for order in self:
+            for line in order.order_line:
+                if line.account_analytic_id:
+                    return super(PurchaseOrder, self).button_approve(
+                        force=False)
+                else:
+                    raise exceptions.UserError(
+                        _('An Analytic Account is required for Validation!'))
