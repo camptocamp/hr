b0d814fdd0ee3448e5553f37552580cc9db8307f
Damien Crier <damien.crier@camptocamp.com>
2017-02-22 15:31:29 +0100

    configure subscription template and sale template

--- odoo/songs/update/update_9_6_0/sale.py
+++ odoo/songs/update/update_9_6_0/sale.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import anthem
+from anthem.lyrics.records import create_or_update
 
 
 @anthem.log
@@ -29,8 +30,20 @@ def configure_sale_app(ctx):
 
 
 @anthem.log
+def configure_contract_template(ctx):
+    vals = {'name': 'Contract template',
+            'type': 'template',
+            'state': 'open'
+            }
+    xml_id = '__setup__.subscription_tmpl_1'
+    create_or_update(ctx, 'sale.subscription', xml_id, vals)
+
+    sale_tmpl = ctx.env.ref('website_quote.website_quote_template_default')
+    sale_tmpl.contract_template = ctx.env.ref(xml_id).id
+
+
+@anthem.log
 def main(ctx):
     """ Configuring sales """
-    # activate_multicurrency(ctx)
     configure_sale_app(ctx)
-    # create_bank_accounts(ctx)
+    configure_contract_template(ctx)
