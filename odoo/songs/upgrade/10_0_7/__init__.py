ee63194cdf185940f4ad3e127fe75a4e01fc3706
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-03-03 16:08:37 +0100

    Rename CRM Stage 5

---
+++ odoo/songs/upgrade/10_0_7/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def rename_crm_stage5(ctx):
+    ctx.env.ref('__setup__.crm_stage_quote').name = '5. Proposal / Quote / JDA'
+
+
+def pre(ctx):
+    """ Pre-update """
+    rename_crm_stage5(ctx)
