cbaa574c5defe84e273499e469544ca38c177672
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-03-10 17:34:16 +0100

    Rewrite specific_sale Do triple validation instead of double Hide Send By Email button for states prior to confirmed

--- odoo/local-src/specific_sale/__openerp__.py
+++ odoo/local-src/specific_sale/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {'name': 'Specific sales',
- 'summary': "Double validation workflow",
+ 'summary': "Triple validation workflow",
  'version': '9.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'maintainer': 'Camptocamp',
---
+++ odoo/local-src/specific_sale/models/company.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from openerp import fields, models
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    so_double_validation = fields.Selection(
+        selection_add=[
+            ('bso_three_step',
+             'Get 3 levels of approvals to confirm a sale order (BSO)')]
+    )
