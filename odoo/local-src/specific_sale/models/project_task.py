51f25a4b0bf04f640606b8415d2bb7fdfefc475a
Denis Leemann <denis.leemann@camptocamp.com>
2017-03-28 10:57:15 +0200

    BOM readonly on task view & smartbutton

--- odoo/local-src/specific_sale/models/project_task.py
+++ odoo/local-src/specific_sale/models/project_task.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProjectTask(models.Model):
@@ -14,3 +14,19 @@ class ProjectTask(models.Model):
         'project_task_id',
         string='BOM',
     )
+    bom_count = fields.Integer(
+        compute='_compute_linked_bom',
+        string='Count BOM',
+    )
+
+    def action_view_bom(self):
+        action = self.env.ref('mrp.mrp_bom_form_action')
+        result = action.read()[0]
+        result['context'] = {'default_project_task_id': self.id}
+        return result
+
+    def _compute_linked_bom(self):
+        BOM = self.env['mrp.bom']
+        for task in self:
+            task.bom_count = BOM.search_count([
+                ('project_task_id', '=', task.id)])
