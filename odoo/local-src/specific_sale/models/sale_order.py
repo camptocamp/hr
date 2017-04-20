89f9aed1edbab89bf94096887ef09bdc7de4042c
Denis Leemann <denis.leemann@camptocamp.com>
2017-04-20 15:58:44 +0200

    flake8

--- odoo/local-src/specific_sale/models/sale_order.py
+++ odoo/local-src/specific_sale/models/sale_order.py
@@ -234,7 +234,8 @@ class SaleOrder(models.Model):
     @api.multi
     def action_quotation_send(self):
         '''
-        This function opens a window to compose an email, with the edi sale template message loaded by default
+        This function opens a window to compose an email, with the edi sale
+        template message loaded by default
         '''
         self.ensure_one()
         res = super(SaleOrder, self).action_quotation_send()
@@ -243,11 +244,14 @@ class SaleOrder(models.Model):
 
         ir_model_data = self.env['ir.model.data']
         try:
-            template_id = ir_model_data.get_object_reference('specific_sale', 'email_template_edi_fin_quot')[1]
+            template_id = ir_model_data.get_object_reference(
+                'specific_sale',
+                'email_template_edi_fin_quot')[1]
         except ValueError:
             template_id = res['context']['default_template_id'] or False
 
         ctx = dict()
+        cust_layout = ('sale.mail_template_data_notification_email_sale_order')
         ctx.update({
             'default_model': 'sale.order',
             'default_res_id': self.ids[0],
@@ -255,7 +259,7 @@ class SaleOrder(models.Model):
             'default_template_id': template_id,
             'default_composition_mode': 'comment',
             'mark_so_as_sent': True,
-            'custom_layout': "sale.mail_template_data_notification_email_sale_order"
+            'custom_layout': cust_layout,
         })
         res['context'] = ctx
         return res
