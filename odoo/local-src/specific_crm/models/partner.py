ce96ee11773860887ad0d3ad7856bebdb09a45c6
Denis Leemann <denis.leemann@camptocamp.com>
2017-04-20 18:33:00 +0200

    base model for licenses
    Add ACL
    
    views

--- odoo/local-src/specific_crm/models/partner.py
+++ odoo/local-src/specific_crm/models/partner.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, _
 
 
 class ResPartner(models.Model):
@@ -16,6 +16,11 @@ class ResPartner(models.Model):
         compute='_compute_opp_nda',
         readonly=True,
     )
+    customer_license_ids = fields.One2many(
+        'res.partner.license',
+        'partner_id',
+        string='Licenses',
+    )
 
     def _compute_opp_nda(self):
         for cust in self:
@@ -26,3 +31,33 @@ class ResPartner(models.Model):
             nda_ids = self.env['crm.lead'].search(domain)
             for nda_id in nda_ids:
                 cust.opp_nda_ids |= nda_id
+
+
+class ResPartnerLicense(models.Model):
+    _name = 'res.partner.license'
+
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Customer',
+    )
+    license_type = fields.Selection([
+        ('dev', _('Dev')),
+        ('production', _('Production')),
+        ('test_demo', _('Test / Demo')),
+        ('dev_prod_opt', _('Dev + Production Option')),
+    ])
+    start_date = fields.Date(
+        string='Start Date',
+    )
+    end_date = fields.Date(
+        string='End Date',
+    )
+    area = fields.Char(
+        string='Area',
+    )
+    domain = fields.Char(
+        string='Domain',
+    )
+    license_comment = fields.Text(
+        string='Comments',
+    )
