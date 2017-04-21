5cfc78074b297affa677fc57c6ae37d8bd52f10b
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2017-04-21 17:25:29 +0200

    enable sales song

--- odoo/songs/install/sale.py
+++ odoo/songs/install/sale.py
@@ -37,14 +37,9 @@ def configure_sale_double_validation(ctx):
 @anthem.log
 def configure_contract_template(ctx):
     vals = {'name': 'Contract template',
-            'type': 'template',
-            'state': 'open'
             }
     xml_id = '__setup__.subscription_tmpl_1'
-    create_or_update(ctx, 'sale.subscription', xml_id, vals)
-
-    sale_tmpl = ctx.env.ref('website_quote.website_quote_template_default')
-    sale_tmpl.contract_template = ctx.env.ref(xml_id).id
+    create_or_update(ctx, 'sale.subscription.template', xml_id, vals)
 
 
 @anthem.log
