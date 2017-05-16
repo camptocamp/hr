3b13d73b05735d99ecc5e2c74c49fccd03a8c15c
Denis Leemann <denis.leemann@camptocamp.com>
2017-05-16 17:12:13 +0200

    Create employee & address_home if needed
    Create employee & address_home if needed

---
+++ odoo/local-src/specific_expense/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "RocTool specific expense",
+    "version": "10.0.1.0.0",
+    "depends": ['hr',
+                ],
+    "author": "Camptocamp",
+    "website": "http://www.camptocamp.com",
+    "license": "AGPL-3",
+    "category": "Expense",
+    "data": ['data/res_partner_category.xml',
+             'views/employee_views.xml',
+             ],
+    'installable': True,
+}
---
+++ odoo/local-src/specific_expense/models/hr_employee.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class Employee(models.Model):
+    _inherit = 'hr.employee'
+
+    expense_refund_account_id = fields.Many2one(
+        'account.account',
+        string='Expense Refund Account',
+        related='address_home_id.property_account_payable_id',
+        store=False,
+        readonly=False,
+    )
+
+    @api.model
+    def create(self, vals):
+        if vals['address_home_id']:
+            return super(Employee, self).create(vals)
+        country_id = None
+        if not vals['country_id']:
+            Company = self.env['res.company']
+            country_id = Company.browse(vals['company_id']).country_id.id
+        Partner = self.env['res.partner']
+        category_id = self.env.ref('specific_expense.employee_home').id
+        vals_address = {
+            'name': vals.get('name'),
+            'supplier': True,
+            'customer': False,
+            'country_id': country_id if country_id else vals.get('country_id'),
+            'category_id': [(4, category_id)],
+        }
+        partner = Partner.create(vals_address)
+        vals['address_home_id'] = partner.id
+        return super(Employee, self).create(vals)
