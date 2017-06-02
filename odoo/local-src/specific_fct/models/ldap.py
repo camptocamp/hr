148c88bb9391bb394dbaad0bd9d67a6eb7ca75cf
Damien Crier <damien.crier@camptocamp.com>
2017-06-02 14:39:59 +0200

    fix potential utf8 errors

--- odoo/local-src/specific_fct/models/ldap.py
+++ odoo/local-src/specific_fct/models/ldap.py
@@ -13,10 +13,21 @@ class ResCompanyLDAP(models.Model):
     def map_ldap_attributes(self, conf, login, ldap_entry):
         values = super(ResCompanyLDAP, self).map_ldap_attributes(
             conf, login, ldap_entry)
+
+        for key in values:
+            values[key] = values[key].decode('utf-8')
+
         mapping = [
             ('name', ['givenName', 'sn']),
         ]
         for value_key, conf_name in mapping:
-            values[value_key] = ' '.join(
-                [ldap_entry[1][c][0] for c in conf_name])
+            values[value_key] = u' '.join(
+                [ldap_entry[1][c][0].decode('utf-8') for c in conf_name])
+
         return values
+
+    @api.model
+    def get_or_create_user(self, conf, login, ldap_entry):
+        return super(ResCompanyLDAP, self.with_context(
+            no_reset_password=True  # avoid to send mail on user creation
+        )).get_or_create_user(conf, login, ldap_entry)
