ce28d0f1823f522b1a991ce6cbd0712f3f7c5f96
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2017-08-11 09:38:46 +0200

    fixup! Handle invoicing process for MRC products

--- odoo/local-src/specific_sale/wizard/mrp_invoicing.py
+++ odoo/local-src/specific_sale/wizard/mrp_invoicing.py
@@ -14,17 +14,18 @@ class MrpInvoicing(models.TransientModel):
     )
 
     @api.multi
-    def ok(self):
-        for wizard in self:
-            sale_order_id = self.env.context.get('active_ids')[0]
-            sale_order = self.env['sale.order'].browse(sale_order_id)
-            for line in sale_order.order_line:
-                line.qty_delivered = line.with_context(ref_date_mrc_delivery=self.ref_date)._get_delivered_qty()
-            res = sale_order.get_create_invoice_action()
-            # The next wizard need the sale order id in the context
-            res['context'] = {
-                    'active_id': sale_order.id,
-                    'active_ids': [sale_order.id],
-                    'ref_date_mrc_delivery': self.ref_date
-                }
+    def button_ok(self):
+        self.ensure_one()
+        sale_order_id = self.env.context.get('active_ids')[0]
+        sale_order = self.env['sale.order'].browse(sale_order_id)
+        for line in sale_order.order_line:
+            line.qty_delivered = line.with_context(
+                    ref_date_mrc_delivery=self.ref_date)._get_delivered_qty()
+        res = sale_order.get_create_invoice_action()
+        # The next wizard need the sale order id in the context
+        res['context'] = {
+                'active_id': sale_order.id,
+                'active_ids': [sale_order.id],
+                'ref_date_mrc_delivery': self.ref_date
+            }
         return res
