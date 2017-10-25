69f867484f1bb0d55d0bfce13f6ade4784d4073a
Damien Crier <damien.crier@camptocamp.com>
2017-10-25 13:44:01 +0200

    fix tests

--- odoo/local-src/bso_purchase/tests/test_purchase.py
+++ odoo/local-src/bso_purchase/tests/test_purchase.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from . import common
+from odoo import fields
+from dateutil.relativedelta import relativedelta
 
 
 class TestPurchase(common.TestBsoPurchaseCommon):
@@ -26,9 +28,9 @@ class TestPurchase(common.TestBsoPurchaseCommon):
         picking2 = self.po2.picking_ids[0]
         picking2.do_transfer()
 
-        # There should be only one invoice
+        # There should be zero invoices as no end date for subscr
         inv_ids = self.AccountInvoice.po_auto_invoice()
-        self.assertEqual(1, len(inv_ids))
+        self.assertEqual(0, len(inv_ids))
         # if we do it twice there should be no new invoice
         inv_ids = self.AccountInvoice.po_auto_invoice()
         self.assertEqual(0, len(inv_ids))
@@ -46,7 +48,53 @@ class TestPurchase(common.TestBsoPurchaseCommon):
         picking2 = self.po2.picking_ids[0]
         picking2.do_transfer()
 
-        # There should be two invoices
+        # There should be zero invoices as no end date for subscr
+        inv_ids = self.AccountInvoice.po_auto_invoice()
+        self.assertEqual(0, len(inv_ids))
+
+        # There should be no new invoice
+        inv_ids = self.AccountInvoice.po_auto_invoice()
+        self.assertEqual(0, len(inv_ids))
+
+    def test_auto_invoicing_group_end_date(self):
+        self.partner_id.write({
+            'group_supplier_invoice': True,
+            'automatic_supplier_invoicing': True,
+        })
+        self.po.button_confirm()
+        self.po2.button_confirm()
+        end = fields.Date.today() + relativedelta(months=3)
+        self.po.subscr_date_end = end
+        self.po2.subscr_date_end = end
+        picking = self.po.picking_ids[0]
+        picking.do_transfer()
+        picking2 = self.po2.picking_ids[0]
+        picking2.do_transfer()
+
+        # There should be 1 invoice
+        inv_ids = self.AccountInvoice.po_auto_invoice()
+        self.assertEqual(1, len(inv_ids))
+        # if we do it twice there should be no new invoice
+        inv_ids = self.AccountInvoice.po_auto_invoice()
+        self.assertEqual(0, len(inv_ids))
+
+    def test_auto_invoicing_no_group_end_date(self):
+        self.partner_id.write({
+            'group_supplier_invoice': False,
+            'automatic_supplier_invoicing': True,
+        })
+        # Prepare the PO to be invoiced
+        self.po.button_confirm()
+        self.po2.button_confirm()
+        end = fields.Date.today() + relativedelta(months=3)
+        self.po.subscr_date_end = end
+        self.po2.subscr_date_end = end
+        picking = self.po.picking_ids[0]
+        picking.do_transfer()
+        picking2 = self.po2.picking_ids[0]
+        picking2.do_transfer()
+
+        # There should be 2 invoices
         inv_ids = self.AccountInvoice.po_auto_invoice()
         self.assertEqual(2, len(inv_ids))
 
