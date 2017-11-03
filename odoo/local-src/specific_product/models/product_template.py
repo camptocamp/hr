7d4995feccc61d539be5cba73bd718ff577a1d84
Damien Crier <damien.crier@camptocamp.com>
2017-11-03 14:27:04 +0100

    add constraint and onchange uom for subscription products

--- odoo/local-src/specific_product/models/product_template.py
+++ odoo/local-src/specific_product/models/product_template.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields, _
+from odoo import api, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -14,19 +14,25 @@ class ProductTemplate(models.Model):
             return self.env.ref('specific_product.product_unit_month',
                                 raise_if_not_found=False)
 
-    uom_id = fields.Many2one(
-        default=lambda self: self._compute_default_uomid()
-    )
+#    uom_id = fields.Many2one(
+#        default=lambda self: self._compute_default_uomid()
+#    )
 
     @api.onchange('recurring_invoice')
     def set_invoicing_policy(self):
         if self.recurring_invoice:
             self.invoice_policy = 'delivery'
+            uom = self._compute_default_uomid()
+            self.uom_id = uom.id
+            self.po_uom_id = uom.id
         else:
             self.invoice_policy = 'order'
 
-    @api.constrains('uom_id', 'recurring_invoice')
+    @api.constrains('uom_id', 'recurring_invoice', 'invoice_policy')
     def _check_categ_id(self):
         if not (self.recurring_invoice == self.uom_id.recurring):
             raise ValidationError(_('The unit of measure is not compatible '
                                     'with the subscription type'))
+        if self.recurring_invoice and self.invoice_policy != 'delivery':
+            raise ValidationError(_('The recurring products must be invoiced '
+                                    'on delivered quantities'))
