9c793ec62877bde04bb4965ae6f2338003c12d94
Damien Crier <damien.crier@camptocamp.com>
2017-12-04 15:20:43 +0100

    remove unused code

--- odoo/local-src/specific_product/models/__init__.py
+++ odoo/local-src/specific_product/models/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import sale_order
-from . import product
-from . import account_invoice
-from . import network
-from . import network_api
-from . import res_company
 from . import stock
 from . import product_uom_categ
 from . import product_template
