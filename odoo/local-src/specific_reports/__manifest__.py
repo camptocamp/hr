2d9358a55c1b83ba13dbf04b658908b4e7ff1a0d
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2018-01-25 12:01:44 +0100

    customize invoice PDF
    rename some fields in english and french
    reorganise the footer.

---
+++ odoo/local-src/specific_reports/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "BSO specific reports module",
+    "version": "10.0.1.0.0",
+    "depends": [
+        'sale',
+        'report',
+    ],
+    "author": "Camptocamp SA",
+    "website": "http://www.camptocamp.com",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "data": [
+        'templates/layouts.xml',
+        'templates/customer_invoice.xml',
+    ],
+    'installable': True,
+}
