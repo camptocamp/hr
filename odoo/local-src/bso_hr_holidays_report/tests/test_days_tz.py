8ffc1bb546a32d1685d43fbaa88694f76e2d7215
Gaspard Coudurier <gaspard.coudurier@bsonetwork.com>
2018-05-29 17:43:37 +0200

    tests

--- odoo/local-src/bso_hr_holidays_report/tests/test_days_tz.py
+++ odoo/local-src/bso_hr_holidays_report/tests/test_days_tz.py
@@ -20,40 +20,52 @@ class TestDays(TransactionCase):
 
     def _assert_days(self, tz):
         scenarios = [
-            # Overlap on previous month
-            # Should begin at 2018-05-02 00:00:00 as 01 is day off in FR
-            ('2018-04-23 14:00:00', '2018-05-03 13:00:00', 1.5),
-            ('2018-04-23 09:00:00', '2018-05-03 19:00:00', 2),
+            # Overlap on previous month (should begin at 2017-09-03 00:00:00)
+            ('2017-08-30 14:00:00', '2017-09-04 13:00:00', 1.5),
+            ('2017-08-30 09:00:00', '2017-09-04 19:00:00', 2),
             # Same month
-            ('2018-05-22 14:00:00', '2018-05-25 13:00:00', 3),
-            ('2018-05-22 14:00:00', '2018-05-25 19:00:00', 3.5),
-            ('2018-05-22 09:00:00', '2018-05-25 13:00:00', 3.5),
-            ('2018-05-22 09:00:00', '2018-05-25 19:00:00', 4),
-            # Overlap on next month
-            # Should stop at 2018-05-31 23:59:59
-            ('2018-05-24 14:00:00', '2018-06-04 13:00:00', 5.5),
-            ('2018-05-24 09:00:00', '2018-06-04 19:00:00', 6),
+            ('2017-09-18 14:00:00', '2017-09-21 13:00:00', 3),
+            ('2017-09-18 14:00:00', '2017-09-21 19:00:00', 3.5),
+            ('2017-09-18 09:00:00', '2017-09-21 13:00:00', 3.5),
+            ('2017-09-18 09:00:00', '2017-09-21 19:00:00', 4),
+            # Overlap on next month (should stop at 2017-09-29 23:59:59)
+            ('2017-09-22 14:00:00', '2017-10-03 13:00:00', 5.5),
+            ('2017-09-22 09:00:00', '2017-10-03 19:00:00', 6),
+            # Over month
+            ('2017-08-30 09:00:00', '2017-10-03 19:00:00', 21)
         ]
-        holidays = []
-        idx = 0
+        created_holidays = []
         for start_date, end_date, expected_days in scenarios:
-            name = 'user_%s' % idx
-            employee = self._create_employee(name, tz)
-            holiday = self._create_holiday(employee, start_date, end_date)
-            holidays.append((holiday, expected_days))
-            idx += 1
+            holiday_id = self._create_holiday(start_date, end_date, tz)
+            created_holidays.append((holiday_id, expected_days))
 
         self.env['hr.holidays.report'].create({
-            'year': 2018,
-            'month': 5,
+            'year': 2017,
+            'month': 9,
             'last_create': False
         })
 
-        for holiday, expected_days in holidays:
-            reported_days = self._get_reported_days(holiday)
-            self.assertEqual(expected_days, reported_days)
+        for holiday_id, expected_days in created_holidays:
+            reported_days = self._get_reported_days(holiday_id)
+            self.assertEqual(expected_days, reported_days,
+                             holiday_id.employee_id.name)
 
-    def _create_employee(self, name, tz):
+    def _create_holiday(self, start_date, end_date, tz):
+        employee_id = self._create_employee(start_date, end_date, tz)
+        holiday_status_id = self._create_holiday_status()
+        holiday_id = self.env['hr.holidays'].create({
+            'type': 'remove',
+            'employee_id': employee_id.id,
+            'holiday_status_id': holiday_status_id.id,
+            'date_from': self._to_utc_naive(start_date, employee_id),
+            'date_to': self._to_utc_naive(end_date, employee_id)
+        })
+        holiday_id._onchange_date_from()
+        holiday_id.action_validate()
+        return holiday_id
+
+    def _create_employee(self, start_date, end_date, tz):
+        name = "%s %s" % (start_date, end_date)
         user_id = self.env['res.users'].with_context({
             'tracking_disable': True,
             'no_reset_password': True,
@@ -67,28 +79,16 @@ class TestDays(TransactionCase):
         })
         employee_id = self.env['hr.employee'].create({
             'name': name,
-            'user_id': user_id.id,
-            'address_id': self._get_company().partner_id.id,
+            'user_id': user_id.id
         })
         return employee_id
 
-    def _get_company(self, name='BSO Network Solutions SAS'):
-        return self.env['res.company'].search([('name', '=', name)])[0]
-
-    def _create_holiday(self, employee_id, start_date, end_date):
-        holiday_id = self.env['hr.holidays'].create({
-            'type': 'remove',
-            'employee_id': employee_id.id,
-            'holiday_status_id': self._get_holiday_status().id,
-            'date_from': self._to_utc_naive(start_date, employee_id),
-            'date_to': self._to_utc_naive(end_date, employee_id)
+    def _create_holiday_status(self):
+        return self.env['hr.holidays.status'].create({
+            'name': 'Test Holiday',
+            'exclude_rest_days': True,
+            'limit': True
         })
-        holiday_id._onchange_date_from()
-        holiday_id.action_validate()
-        return holiday_id
-
-    def _get_holiday_status(self, name='FR Travail à distance'):
-        return self.env['hr.holidays.status'].search([('name', '=', name)])[0]
 
     def _to_utc_naive(self, date_str, employee_id):
         """Odoo stores Datetime as UTC"""
