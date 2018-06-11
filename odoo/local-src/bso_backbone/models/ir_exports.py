6f6e5717781cd004fcb5f19b8477043940db21cb
Gaspard Coudurier <gaspard.coudurier@bsonetwork.com>
2018-06-11 17:14:04 +0200

    Disable exports on Backbone if not manager

---
+++ odoo/local-src/bso_backbone/models/ir_exports.py
@@ -0,0 +1,18 @@
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+
+class IrExports(models.Model):
+    _inherit = "ir.exports"
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None,
+                    order=None):
+        for _domain in domain:
+            field, operator, resource = _domain
+            if field == 'resource' and resource.startswith('backbone.'):
+                group_manager = 'bso_backbone.group_backbone_manager'
+                if not self.env.user.has_group(group_manager):
+                    raise UserError(_("Export disabled for this resource"))
+        return super(IrExports, self).search_read(domain, fields, offset,
+                                                  limit, order)
