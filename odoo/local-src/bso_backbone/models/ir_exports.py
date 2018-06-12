1ceaead0384605140c933924aaa3572145593e94
Gaspard Coudurier <gaspard.coudurier@bsonetwork.com>
2018-06-12 17:36:57 +0100

    str on resource

--- odoo/local-src/bso_backbone/models/ir_exports.py
+++ odoo/local-src/bso_backbone/models/ir_exports.py
@@ -10,7 +10,7 @@ class IrExports(models.Model):
                     order=None):
         for _domain in domain:
             field, operator, resource = _domain
-            if field == 'resource' and resource.startswith('backbone.'):
+            if field == 'resource' and str(resource).startswith('backbone.'):
                 group_manager = 'bso_backbone.group_backbone_manager'
                 if not self.env.user.has_group(group_manager):
                     raise UserError(_("Export disabled for this resource"))
