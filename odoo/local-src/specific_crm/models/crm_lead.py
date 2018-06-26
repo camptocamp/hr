6dadce9e34d18ccb59cd77315a63027af64f0655
Timon Tschanz <timon.tschanz@gmail.com>
2018-06-26 09:08:09 +0200

    BSIBSO-1063: Remove buttons on leads. (#193)

--- odoo/local-src/specific_crm/models/crm_lead.py
+++ odoo/local-src/specific_crm/models/crm_lead.py
@@ -84,22 +84,6 @@ class CrmLead(models.Model):
         string="Sum planned revenue mrc",
         compute="_compute_sum_nrc_mrc",
     )
-    sum_quotes = fields.Float(
-        string="Sum qoutes",
-        compute="_compute_sum_quotes_sales",
-    )
-    sum_sales = fields.Float(
-        string="Sum sales",
-        compute="_compute_sum_quotes_sales",
-    )
-
-    @api.multi
-    def _compute_sum_quotes_sales(self):
-        for record in self:
-            sales = record._get_so()
-            quotes = record._get_quotes()
-            record.sum_sales = sum(sales.mapped('amount_total'))
-            record.sum_quotes = sum(quotes.mapped('amount_total'))
 
     @api.multi
     @api.depends('child_leads_ids')
@@ -178,52 +162,4 @@ class CrmLead(models.Model):
             ],
             'type': 'ir.actions.act_window',
             'context': {'default_type': 'opportunity'}
-        }
-
-    def _get_quotes(self):
-        self.ensure_one()
-        quotations = self.env['sale.order'].search([
-            '&',
-            ('state', '=', 'draft'),
-            ('commercial_partner_id', '=', self.partner_id.id),
-        ])
-        return quotations
-
-    def _get_so(self):
-        self.ensure_one()
-        sale_orders = self.env['sale.order'].search([
-            '&',
-            ('state', '!=', 'draft'),
-            ('commercial_partner_id', '=', self.partner_id.id),
-        ])
-        return sale_orders
-
-    @api.multi
-    def action_view_quotations(self):
-        self.ensure_one()
-        quotations = self._get_quotes()
-        return {
-            "name": _("Quotes"),
-            "type": "ir.actions.act_window",
-            "res_model": "sale.order",
-            "res_id": self.id,
-            "view_type": "form",
-            "view_mode": "tree,form",
-            "domain": [('id', 'in', quotations.ids)],
-            "context": {"create": False, "show_sale": True},
-        }
-
-    @api.multi
-    def action_view_so(self):
-        self.ensure_one()
-        sale_orders = self._get_so()
-        return {
-            "name": _("Sale Orders"),
-            "type": "ir.actions.act_window",
-            "res_model": "sale.order",
-            "res_id": self.id,
-            "view_type": "form",
-            "view_mode": "tree,form",
-            "domain": [('id', 'in', sale_orders.ids)],
-            "context": {"create": False, "show_sale": True},
         }
