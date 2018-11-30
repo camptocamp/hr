b4823fdebb4059787592b6315b85d9ae2426c053
Mariem ELBERRIRI - BSO <mariem.elberriri@bsonetwork.com>
2018-11-30 16:25:03 +0100

    rules corrected and security file moved to security folder

--- odoo/local-src/bso_restrict_attachments_visibility/__manifest__.py
+++ odoo/local-src/bso_restrict_attachments_visibility/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': 'BSO restrict attachments visibility',
-    'description': 'Restrict access to attachments based on group users',
+    'description': 'restrict access to attachments based on group users',
     'author': 'BSO',
     'website': 'https://www.bsonetwork.com',
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-        'views/attachment_security.xml',
+        'security/attachment_security.xml'
     ],
     'application': False,
 }
