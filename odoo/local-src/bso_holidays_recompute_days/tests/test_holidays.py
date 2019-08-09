01b4bbaca271b9da196edf10e5ba21e7ccd6b9d8
Mariem ELBERRIRI - BSO <mariem.elberriri@bsonetwork.com>
2019-08-09 12:24:32 +0100

    Tests updated

--- odoo/local-src/bso_holidays_recompute_days/tests/test_holidays.py
+++ odoo/local-src/bso_holidays_recompute_days/tests/test_holidays.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from odoo import fields
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
+from pytz import timezone
 
 
 class TestHolidays(TransactionCase):
@@ -61,185 +63,121 @@ class TestHolidays(TransactionCase):
     def test_holidays_case1(self):
         date_from = '2019-06-04 08:00:00'
         date_to = '2019-06-04 18:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -1)
 
     def test_holidays_case2(self):
         date_from = '2019-06-04 08:00:00'
         date_to = '2019-06-04 13:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -0.5)
 
     def test_holidays_case3(self):
         date_from = '2019-06-04 14:00:00'
         date_to = '2019-06-04 18:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -0.5)
 
     def test_holidays_case4(self):
         date_from = '2019-06-04 08:00:00'
         date_to = '2019-06-06 18:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -3)
 
     def test_holidays_case5(self):
         date_from = '2019-06-04 14:00:00'
         date_to = '2019-06-06 18:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -2.5)
 
     def test_holidays_case6(self):
         date_from = '2019-06-04 08:00:00'
         date_to = '2019-06-06 13:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -2.5)
 
     def test_holidays_case7(self):
         date_from = '2019-06-10 08:00:00'
         date_to = '2019-06-10 18:00:00'
         with self.assertRaises(ValidationError):
-            self.holidays_obj.create({
-                'name': 'Travail à distance',
-                'holiday_status_id': self.status.id,
-                'date_from': date_from,
-                'date_to': date_to,
-                'employee_id': self.employee.id,
-                'type': 'remove',
-            })
+            self._create_holiday(name='Travail à distance',
+                                 date_from=date_from,
+                                 date_to=date_to,
+                                 holiday_type='remove')
 
     def test_holidays_case8(self):
         date_from = '2019-06-10 08:00:00'
         date_to = '2019-06-10 13:00:00'
         with self.assertRaises(ValidationError):
-            self.holidays_obj.create({
-                'name': 'Travail à distance',
-                'holiday_status_id': self.status.id,
-                'date_from': date_from,
-                'date_to': date_to,
-                'employee_id': self.employee.id,
-                'type': 'remove',
-            })
+            self._create_holiday(name='Travail à distance',
+                                 date_from=date_from,
+                                 date_to=date_to,
+                                 holiday_type='remove')
 
     def test_holidays_case9(self):
         date_from = '2019-06-10 14:00:00'
         date_to = '2019-06-10 18:00:00'
         with self.assertRaises(ValidationError):
-            self.holidays_obj.create({
-                'name': 'Travail à distance',
-                'holiday_status_id': self.status.id,
-                'date_from': date_from,
-                'date_to': date_to,
-                'employee_id': self.employee.id,
-                'type': 'remove',
-            })
+            self._create_holiday(name='Travail à distance',
+                                 date_from=date_from,
+                                 date_to=date_to,
+                                 holiday_type='remove')
 
     def test_holidays_case10(self):
         date_from = '2019-06-07 08:00:00'
         date_to = '2019-06-11 18:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -2)
 
     def test_holidays_case11(self):
         date_from = '2019-06-08 14:00:00'
         date_to = '2019-06-11 18:00:00'
-
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'date_from': date_from,
-            'date_to': date_to,
-            'employee_id': self.employee.id,
-            'type': 'remove',
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       date_from=date_from,
+                                       date_to=date_to,
+                                       holiday_type='remove')
         self.assertEqual(holiday.number_of_days, -1)
 
     def test_holidays_case12(self):
         date_from = '2019-06-08 14:00:00'
         date_to = '2019-06-09 18:00:00'
         with self.assertRaises(ValidationError):
-            self.holidays_obj.create({
-                'name': 'Travail à distance',
-                'holiday_status_id': self.status.id,
-                'date_from': date_from,
-                'date_to': date_to,
-                'employee_id': self.employee.id,
-                'type': 'remove',
-            })
+            self._create_holiday(name='Travail à distance',
+                                 date_from=date_from,
+                                 date_to=date_to,
+                                 holiday_type='remove')
 
     def test_holidays_allocation_0_days(self):
         with self.assertRaises(ValidationError):
-            self.holidays_obj.create({
-                'name': 'Travail à distance',
-                'holiday_status_id': self.status.id,
-                'employee_id': self.employee.id,
-                'type': 'add',
-                'number_of_days_temp': 0
-            })
+            self._create_holiday(name='Travail à distance',
+                                 holiday_type='add',
+                                 number_of_days_temp=0)
 
     def test_holidays_allocation_10_days(self):
-        holiday = self.holidays_obj.create({
-            'name': 'Travail à distance',
-            'holiday_status_id': self.status.id,
-            'employee_id': self.employee.id,
-            'type': 'add',
-            'number_of_days_temp': 10
-        })
+        holiday = self._create_holiday(name='Travail à distance',
+                                       holiday_type='add',
+                                       number_of_days_temp=10)
         self.assertEqual(holiday.number_of_days, 10)
 
     def test_create_holiday_without_leave_balance(self):
@@ -248,11 +186,33 @@ class TestHolidays(TransactionCase):
         date_from = '2019-06-08 14:00:00'
         date_to = '2019-06-11 18:00:00'
         with self.assertRaises(ValidationError):
-            self.holidays_obj.create({
-                'name': 'Sick leave',
-                'holiday_status_id': status_id.id,
-                'date_from': date_from,
-                'date_to': date_to,
-                'employee_id': self.employee.id,
-                'type': 'remove',
-            })
+            self._create_holiday(name='Sick leave',
+                                 date_from=date_from,
+                                 date_to=date_to,
+                                 status_id=status_id.id,
+                                 holiday_type='remove')
+
+    def _create_holiday(self, **kwargs):
+        date_from_tz = self._to_utc(kwargs.get('date_from'))
+        date_to_tz = self._to_utc(kwargs.get('date_to'))
+        holiday_values = {
+            'name': kwargs.get('name'),
+            'holiday_status_id': kwargs.get('status_id', self.status.id),
+            'date_from': date_from_tz,
+            'date_to': date_to_tz,
+            'employee_id': self.employee.id,
+            'type': kwargs.get('holiday_type'),
+        }
+        if kwargs.get('holiday_type') == 'add':
+            holiday_values.update({
+                'number_of_days_temp': kwargs.get('number_of_days_temp')})
+
+        return self.holidays_obj.create(holiday_values)
+
+    def _to_utc(self, date_str):
+        if not date_str:
+            return False
+        dt = fields.Datetime.from_string(date_str)
+        user_tz = self.env.user.tz
+        dt_utc = timezone(user_tz).localize(dt).astimezone(timezone('UTC'))
+        return fields.Datetime.to_string(dt_utc.replace(tzinfo=None))
