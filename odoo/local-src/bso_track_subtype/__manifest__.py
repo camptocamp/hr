c52902a778f7b512933a9a8a19c8c67655353295
Mariem ELBERRIRI - BSO <mariem.elberriri@bsonetwork.com>
2019-12-12 16:51:25 +0100

    demo data & tests removed

--- odoo/local-src/bso_track_subtype/__manifest__.py
+++ odoo/local-src/bso_track_subtype/__manifest__.py
@@ -5,13 +5,9 @@
     'author': 'BSO',
     'website': 'https://www.bsonetwork.com',
     'depends': ['base', 'mail'],
-    # ,'sale', 'bso_dealsheet', 'sales_team'],
     'data': [
         'security/ir.model.access.csv',
         'views/mail_message_subtype.xml'
     ],
-    # 'demo': [
-    #     'demo/demo_data.xml',
-    # ],
     'application': False,
 }
