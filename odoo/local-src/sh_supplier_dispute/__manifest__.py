533d41ddf99faffd5040520c3095c4ecb03ae601
Gaspard Coudurier <gaspard.coudurier@bsonetwork.com>
2020-01-21 17:33:32 +0100

    format

--- odoo/local-src/sh_supplier_dispute/__manifest__.py
+++ odoo/local-src/sh_supplier_dispute/__manifest__.py
@@ -6,7 +6,13 @@
     "website": "https://www.softhealer.com",
     "category": "Accounting",
     "summary": "This module is useful to Manage Disputed Supplier.",
-    "description": """This module is useful to Manage Disputed Supplier. If any supplier supplies some damaged goods, late delivery, incomplete delivery that time you can create dispute and stop payment till not solve the problem using this module. This module provides one tab 'dispute' in vendor bill so you can create dispute directly. You can not make register payment till the dispute is not solved.""",
+    "description": """This module is useful to Manage Disputed Supplier. \
+     If any supplier supplies some damaged goods, late delivery, \
+     incomplete delivery that time you can create dispute and stop payment \
+     till not solve the problem using this module. \
+     This module provides one tab 'dispute' in vendor bill so you can \
+     create dispute directly. \
+     You can not make register payment till the dispute is not solved.""",
     "version": "12.0.1",
     "depends": [
         "base",
