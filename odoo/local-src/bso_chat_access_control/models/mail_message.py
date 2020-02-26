143421aeaef9d2f17bdb8c48af28fac2b2870c4e
Mohammed Janati IDRISSI - BSO <mohammed.janatiidrissi@bsonetwork.com>
2020-02-26 11:16:26 +0100

    dont check chat access if no model in vals

--- odoo/local-src/bso_chat_access_control/models/mail_message.py
+++ odoo/local-src/bso_chat_access_control/models/mail_message.py
@@ -1,4 +1,5 @@
-from odoo import models, api, SUPERUSER_ID
+from odoo import models, api, SUPERUSER_ID, _
+from odoo.exceptions import AccessError
 
 
 class MailMessage(models.Model):
@@ -6,19 +7,27 @@ class MailMessage(models.Model):
 
     @api.model
     def create(self, vals):
-        if vals.get('message_type') != 'notification':
-            self.env.cache['target_model'] = vals.get('model')
-            write_access = self.env['ir.model.access'].search([
-                ('perm_write', '=', True),
-                ('group_id', 'in', self.env.user.groups_id.ids),
-                ('model_id', '=', self.env.context.get('default_model'))
-            ])
-            if not write_access:
-                self.env['ir.model.access'].check_chat_write_access()
-                current_uid = self.env.uid
-                try:
-                    self.env.uid = SUPERUSER_ID
-                    return super(MailMessage, self).create(vals)
-                finally:
-                    self.env.uid = current_uid
-        return super(MailMessage, self).create(vals)
+        model = vals.get('model')
+        if not model:
+            return super(MailMessage, self).create(vals)
+
+        chat_access = self._has_chat_access(model)
+        if not chat_access:
+            raise AccessError(
+                _("Sorry, you are not allowed to comment on this document."))
+        cur_uid = self.env.uid
+        try:
+            self.env.uid = SUPERUSER_ID
+            return super(MailMessage, self).create(vals)
+        finally:
+            self.env.uid = cur_uid
+
+    def _has_chat_access(self, model_id):
+        if self._uid == 1:
+            # User root have all accesses
+            return True
+        return bool(self.env['ir.model.access'].search([
+            ('model_id', '=', model_id),
+            ('perm_chat', '=', True),
+            ('group_id', 'in', self.env.user.groups_id.ids),
+        ], limit=1))
