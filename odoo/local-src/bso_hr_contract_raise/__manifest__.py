13c2b76fe9f5949dafbdcb731114020b7856db0c
Mohammed Janati IDRISSI - BSO <mohammed.janatiidrissi@bsonetwork.com>
2020-03-06 15:12:55 +0100

    update security groups/rules

--- odoo/local-src/bso_hr_contract_raise/__manifest__.py
+++ odoo/local-src/bso_hr_contract_raise/__manifest__.py
@@ -7,7 +7,7 @@
     'data': [
         'views/hr_contract_form.xml',
         'views/hr_contract_raise_view.xml',
-        'security/res_groups.xml',
+        #'security/res_groups.xml',
         'security/ir.model.access.csv',
     ],
     'application': False,
