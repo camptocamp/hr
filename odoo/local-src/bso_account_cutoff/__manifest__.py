6e971559d26b5b22d06b93256f67c5f2b6df68e8
Gaspard Coudurier <gaspard.coudurier@gmail.com>
2020-09-15 18:47:31 +0200

    BSO Account Cutoff

---
+++ odoo/local-src/bso_account_cutoff/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'BSO Account Cutoff',
+    'description': 'Partner and single counter-part in Cutoff reports',
+    'author': 'BSO',
+    'website': 'https://www.bsonetwork.com',
+    'depends': [
+        'account_cutoff_base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': False,
+}
