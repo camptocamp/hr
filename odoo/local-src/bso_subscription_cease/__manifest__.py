6c80da87952e064f41a19aa9ecb083809ae54d85
Mohammed Janati IDRISSI - BSO <mohammed.janatiidrissi@bsonetwork.com>
2020-11-13 14:22:45 +0000

    add bso_subscription_cease

---
+++ odoo/local-src/bso_subscription_cease/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Subscription Cease',
+    'category': 'sale',
+    'description': 'Allow cease of a subscription line',
+    'author': 'BSO',
+    'website': 'https://www.bsonetwork.com',
+    'depends': ['bso_sales_process', 'sales_team'],
+    'data': [
+        'views/sale_subscription.xml',
+        'views/replace_subscription_line_wizard.xml',
+        'security/ir.model.access.csv',
+        'views/cease_order.xml'
+    ],
+    'application': False,
+}
