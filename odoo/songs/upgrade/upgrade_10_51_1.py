7778bf16cfb203e08f8388b58ab3e473d8319271
Mariem ELBERRIRI - BSO <mariem.elberriri@bsonetwork.com>
2021-02-11 12:44:54 +0100

    upgrade_10_51_1.py updated

--- odoo/songs/upgrade/upgrade_10_51_1.py
+++ odoo/songs/upgrade/upgrade_10_51_1.py
@@ -4,7 +4,7 @@ import anthem
 
 @anthem.log
 def main(ctx):
-    self.env.cr.execute("""
+    ctx.env.cr.execute("""
 		ALTER TABLE IF EXISTS delivery_project RENAME COLUMN sale_order_id TO order_id;
 		ALTER TABLE IF EXISTS delivery_project RENAME TO delivery_project_so;
 		ALTER TABLE IF EXISTS delivery_project_line rename to delivery_line_so;
