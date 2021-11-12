6e81ef6dd5b9b9450f09936751908a6a4710f7e7
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2021-11-12 11:25:18 +0100

    [14.0][IMP] Add validity_end_date to hr_course model

---
+++ hr_course/models/res_company.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    mailing_list_to_alert = fields.One2many(
+        "res.partner", "company_mailing_list", string="Users to inform course end"
+    )
+    alerting_delay = fields.Integer(
+        string="Alerting delay before end of validity (days)"
+    )
