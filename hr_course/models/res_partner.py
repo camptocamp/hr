fbbaede80fe5eeda878b28164333fd065d1ae1f1
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2021-11-12 11:25:18 +0100

    [14.0][IMP] Add validity_end_date to hr_course model

---
+++ hr_course/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    company_mailing_list = fields.Many2one("res.company")
