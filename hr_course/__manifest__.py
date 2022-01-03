1df3ea2bbde5fe1da6061eb32331303cbc62dc87
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2022-01-03 13:56:57 +0100

    fixup! [14.0][IMP] Add validity_end_date to hr_course model

--- hr_course/__manifest__.py
+++ hr_course/__manifest__.py
@@ -15,6 +15,7 @@
         "security/ir.model.access.csv",
         "data/ir_cron_data.xml",
         "data/mail_template.xml",
+        "data/mail_channel.xml",
         "views/hr_course_category_views.xml",
         "views/hr_course_views.xml",
         "views/hr_employee_views.xml",
